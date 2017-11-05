5fe71da10327fda5a9f758ad83e69b0e5b6bc81d
Alexis <ali.finney@gmail.com>
2017-11-04 17:58:56 -0700

    more boilerplate added

---
+++ README.txt
@@ -0,0 +1,4 @@
+An elegant, minimal eComm site to purchase from your favorite local artist, Gail G. Finney. 
+
+
+Created with love by Alexis Finney
---
+++ controllers/user.js
@@ -0,0 +1,67 @@
+//Update the name of the controller below and rename the file.
+const admin = require("../controllers/admin.js");
+const user = require("../controllers/user.js");
+
+
+module.exports = function(app){
+  app.use(cartCheck);
+  
+  // USER EXPERIENCE
+  app.get('/', user.index);
+
+  app.get('/beads', user.getAll);
+
+  app.get('/beads/:id', user.getOne);
+
+  app.get('/beads/cart:id', user.addToCart);
+
+  app.get('/about', user.about);
+
+  app.get('/contact', user.contact);
+
+  // ADMIN EXPERIENCE
+  app.get('/add', admin.addOne);
+
+  app.post('/add', admin.create);
+
+  app.get('/edit/:id', admin.edit);
+
+  app.post('/edit/:id', admin.update);
+
+  app.get('/delete/:id', admin.delete);
+
+
+  // USER SESSION FUNCTION
+  function userAuth(req, res, next){
+    if(req.session.user || req.session.admin){
+      next();
+    }else{
+      res.redirect("/");
+    }
+  }
+
+  // ADMIN SESSION FUNCTION
+  function adminAuth(req,res,next){
+    if(req.session.admin){
+      next();
+    }else{
+      res.redirect('/');
+    }
+  }
+  function cartCheck(req, res, next){
+    if(!req.session.cart){
+      req.session.cart = [];
+      req.session.save(()=>{
+        next();
+      })
+    }else{
+      next();
+    }
+  }
+
+
+  app.use(function(req, res){
+
+      res.render('404');
+    })
+  }
